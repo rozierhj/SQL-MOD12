58023b44847db131d6d88d41eb5e422f89794acd
rozierhj <160989130+rozierhj@users.noreply.github.com>
2024-07-28 21:07:19 -0400

    all views complete

--- db/seedDB.js
+++ db/seedDB.js
@@ -36,4 +36,52 @@ async function seedDepartments(){
     }
 }
 
-seedDepartments();
+const role = [
+    {title:'accountant',salary:34000,department_id:3},
+    {title:'drafter',salary:20000,department_id:5},
+    {title:'design engineer',salary:40000,department_id:5}
+]
+
+async function seedRoles(){
+    try{
+        await client.connect();
+
+        for (const roles of role){
+            const query = 'INSERT INTO role (title, salary, department_id) VALUES ($1, $2, $3)';
+            await client.query(query, [roles.title, roles.salary, roles.department_id]);
+        }
+    }
+    catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.end();
+    }
+}
+
+const employee = [
+    {first_name:'buffy',last_name:'summers',role_id:2},
+    {first_name:'willow',last_name:'lastName1',role_id:1},
+    {first_name:'xander',last_name:'lastName2',role_id:3}
+];
+
+async function seedEmployees(){
+    try{
+        await client.connect();
+
+        for (const employees of employee){
+            const query = 'INSERT INTO employee (first_name, last_name, role_id) VALUES ($1, $2, $3)';
+            await client.query(query, [employees.first_name, employees.last_name, employees.role_id]);
+        }
+    }
+    catch(err){
+        console.error(err);
+    }
+    finally{
+        await client.end();
+    }
+}
+
+//seedRoles();
+//seedDepartments();
+seedEmployees();
--- index.js
+++ index.js
@@ -12,12 +12,51 @@ const client = new Client({
 async function displayDepartments(){
     try{
 
-        await client.connect();
-
         const departments = await client.query('SELECT * FROM departments');
         console.table(departments.rows);
 
     }
+    catch(err){
+        console.error(err);
+    }
+}
+
+async function displayRoles(){
+    try{
+
+        const query = `SELECT role.id, role.title, departments.name as department, role.salary FROM role JOIN departments ON role.department_id = departments.id`;
+
+        const res = await client.query(query);
+        console.table(res.rows);
+
+    }
+    catch(err){
+        console.error(err);
+    }
+}
+
+async function displayEmployees(){
+    try{
+
+        const query = `
+        SELECT 
+       e.id AS id,
+        e.first_name,
+        e.last_name,
+        r.title,
+        d.name AS department,
+        r.salary,
+        COALESCE(m.first_name || ' ' || m.last_name,'No Manager') AS manager
+        FROM employee e
+        JOIN role r ON e.role_id = r.id
+        JOIN departments d ON r.department_id = d.id
+        LEFT JOIN employee m ON e.manager_id = m.id
+        `;
+
+        const res = await client.query(query);
+        console.table(res.rows);
+    }
+
     catch(err){
         console.error(err);
     }
@@ -54,10 +93,14 @@ async function userInput(){
 
                 break;
             case 'view all roles':
-                console.log('view all roles chosen');
+                
+                await displayRoles();
+
                 break;
             case 'view all employees':
-                console.log('view all employees chosen');
+                
+                await displayEmployees();
+
                 break;
             case 'add a department':
                 console.log('add a department chosen');
@@ -86,7 +129,20 @@ async function userInput(){
 
 }
 
-userInput();
+async function main(){
+    try{
+        await client.connect();
+        await userInput();
+    }
+    catch(err){
+        console.error(err);
+        await client.end();
+    }
+}
+
+main();
+
+
 
 
 
